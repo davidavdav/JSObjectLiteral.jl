e1324c27edc45f1fd3933a783098849fb80dcd63
Plasmatium <slv309@163.com>
2019-03-05 00:11:47 +0800

    make this be able to match single symbol (es6)
    ```julia
    date = "2019-3-5"
    @mlstyle {
      date,
    }
    Dict{String,String} with 1 entry:
      "date" => "2019-3-5"
    ```

--- src/mlstyle.jl
+++ src/mlstyle.jl
@@ -2,7 +2,11 @@ using MLStyle
 
 mlstyle(node) = @match node begin
     :({ $(kvs...) }) =>
-        let f = @λ :($k : $v) -> Expr(:call, =>, string(k), mlstyle(v))
+        let f = @λ begin
+                :($k : $v) -> Expr(:call, =>, string(k), mlstyle(v))
+                :($k) -> Expr(:call, =>, string(k), mlstyle(k))
+            end
+            
             Expr(:call, Dict, (f(kv) for kv in kvs if !(kv isa LineNumberNode))...)
         end
     :[$(elts...)] => Expr(:vect, map(mlstyle, elts)...)
